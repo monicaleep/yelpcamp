8aa5f61dbbf50e61af966ac6b149a20a3901aa4e
monicaleep <monica.parrillo@gmail.com>
2020-06-14 20:50:24 -0500

    add auth to comment edit and delete

--- routes/comments.js
+++ routes/comments.js
@@ -47,7 +47,7 @@ router.post("/",isLoggedIn,(req,res)=>{
 
 
 //EDIT route
-router.get("/:comment_id/edit",(req,res)=>{
+router.get("/:comment_id/edit",checkCommentOwnership,(req,res)=>{
   Comment.findById(req.params.comment_id,(err,foundComment)=>{
     if(err){
       res.redirect("back")
@@ -61,13 +61,24 @@ router.get("/:comment_id/edit",(req,res)=>{
 })
 
 //Update route
-router.put("/:comment_id",(req,res)=>{
+router.put("/:comment_id",checkCommentOwnership,(req,res)=>{
   Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,(err,updatedComment)=>{
     if(err){
       console.log(err);
       res.redirect('back');
     } else {
       res.redirect("/campgrounds/"+req.params.id)
+    }
+  })
+})
+
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id",checkCommentOwnership,(req,res)=>{
+  Comment.findByIdAndRemove(req.params.comment_id,(err)=>{
+    if(err){
+      res.redirect('back')
+    } else {
+      res.redirect('/campgrounds/'+req.params.id)
     }
   })
 })
@@ -79,6 +90,28 @@ function isLoggedIn(req, res, next){
     return next();
   } else {
     res.redirect("/login")
+  }
+}
+
+function checkCommentOwnership(req,res,next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id,(err,foundComment)=>{
+      if(err){
+        console.log(err);
+        res.redirect('back')
+      } else{
+        // does user own comment?
+        if(foundComment.author.id.equals(req.user._id)){
+          next()
+        } else {
+          // otherwise redirect
+          res.redirect('back')
+        }
+      }
+    });
+    // if not, redirect
+  } else {
+    res.redirect("back")
   }
 }
 module.exports = router;
